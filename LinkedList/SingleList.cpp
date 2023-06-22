28e14f8585b8c9b49613991a19ae4a8b559eada6
Ernie White <ernie.white.68@gmail.com>
2023-06-22 09:43:27 -0500

    implemented the basis started methods

---
+++ LinkedList/SingleList.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "node.h"
+#include "single.h"
+
+using namespace std;
+
+SingleLinkedList::SingeLinkedList()
+{
+    start = NULL;
+}
+
+inline bool SingleLinkedList::IsEmpty() const
+{
+    return start == NULL;
+}
+
+void SingleLinkedList::displayList() const
+{
+    if (isEmpty())
+    {
+        cout << "List is empty\n";
+        return;
+    }
+
+    cout << "List is :\n";
+    Node *p = start;
+    while (p != NULL)
+    {
+        cout << p->info << ", ";
+        p = p->link;
+    }
+    cout << "\n\n";
+}
+
+int SingleLinkedList::countNodes() const
+{
+    int n = 0;
+    Node *p = start;
+    
+    while (p != NULL)
+    {
+        n++;
+        p = p->link;
+    }
+    return n;
+}
+
+int SingleLinkedList::find(int x) const
+{
+    int position = 1;
+    Node *p = start;
+
+    while (p !- NULL)
+    {
+        if (p->info == x)
+        {
+            return position;
+        }
+        position++;
+    }
+    return 0;
+}
+
+bool SingleLinkedList::isEmpty() const
+{
+    return start == NULL;
+}
+
